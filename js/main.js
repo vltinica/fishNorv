551b88df3a5b45526d57a65ac7aa673ecf1943af
Vladimir Tinica <vltinica@gmail.com>
2022-03-12 22:48:23 -0500

    slider_end

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(function () {
   $(".header-slider").slick({
-    
+    dots: true,
+    // vertical: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="./images/prev.svg" alt="arrow-prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="./images/next.svg" alt="arrow-next"></button>',
   });
 });
